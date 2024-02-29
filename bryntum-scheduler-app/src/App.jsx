4f0853855513344bbbc93c5380da42dc475d29e5
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2024-02-29 00:42:31 +0000

    chg

--- bryntum-scheduler-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { BryntumScheduler } from "@bryntum/scheduler-react";
-import { schedulerConfig } from "./SchedulerConfig";
-import "./App.scss";
-
-function App() {
-  return <BryntumScheduler {...schedulerConfig} />;
-}
-
-export default App;
