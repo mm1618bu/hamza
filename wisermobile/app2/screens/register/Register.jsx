f3acb595ceaaf48f3c51b85ee62da3d11b3c9499
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2025-03-03 10:05:48 +0000

    Push at 405am

--- wisermobile/app2/screens/register/Register.jsx
+++ wisermobile/app2/screens/register/Register.jsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Register() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Register Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Register Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
